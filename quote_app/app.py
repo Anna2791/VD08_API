0c1af04c811988a0ed4b6ac24202f9753552f9c0
Anna2791 <annushka279@mail.ru>
2024-09-17 15:46:05 +0300

    получилось что получилось

--- quote_app/app.py
+++ quote_app/app.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template
+
 import requests
+import ssl
+import urllib3
+
+# Отключаем предупреждения
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Игнорируем проверку SSL
+context = ssl._create_unverified_context()
+response = urllib3.PoolManager(cert_reqs='CERT_NONE', ssl_context=context)
+response = requests.get('https://api.quotable.io/random', verify=False)
+print(response.text)
 
 app = Flask(__name__)
 
